0c35ec0075cedf4ff4efba9164194c2381a8eeed
Rhyper22 <147758052+Rhyper22@users.noreply.github.com>
2023-11-03 08:31:49 -0400

    Add files via upload
    x

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql3 = require ('mysql3')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+console.log(process.env.USER_ADMIN)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var conexion = require('./views/database');
+
+router.get('/', function(req, res, next) {
+  conexion.query('SELECT * FROM Productos', (error, results)=>{
+    if(error){
+      throw error;
+    }else{
+      res.send(results);
+    }
+  });
+});
+
 /* GET home page. */
 router.get('/about', function(req, res, next) {
   res.render('student.ejs', { title: 'Rafael Funes' });
 });
 
-router.get('/', function(req, res, next) {
-  res.render('index.ejs', { title: 'Form_User' });
-});
 
 module.exports = router;

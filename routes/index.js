5b6082aaacc2bc1cac15505b0bd31da3eeed7ce6
Rhyper22 <147758052+Rhyper22@users.noreply.github.com>
2023-11-03 12:19:02 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,22 @@ router.get('/about', function(req, res, next) {
 });
 
 router.get('/', function(req, res, next) {
-  res.render('index.ejs', { title: 'Form_User' });
+  res.render('index.ejs', (error,results) =>{
+    if(error){
+      throw error;
+    }else{
+      res.render('index.ejs', {results:results});
+    }
+  });
+});
+
+//CREAR REGISTROS
+router.get('/create', function(req, res, next) {
+  res.render('create.ejs', { title: 'Creación' });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index.ejs', { title: 'Form_User'});
 });
 
 module.exports = router;

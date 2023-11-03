f2c87f3c4c596e7f7c2fb9ddf71b8e1647f951fb
Rhyper22 <147758052+Rhyper22@users.noreply.github.com>
2023-11-03 15:57:10 -0400

    Add files via upload
    equis

--- database/bd.js
+++ database/bd.js
@@ -1,12 +1,30 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3= require('sqlite3').verbose();
 
-// open the database
-let db = new sqlite3.Database('./Proyecto.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the digital watches products database.');
-});
+let db = new sqlite3.Database(':memory:', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+  });
 
+  db.run("CREATE TABLE IF NOT EXISTS Productos (Productos_id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre Text NOT NULL, Codigo INTEGER NOT NULL, Precio INTEGER NOT NULL, Descripcion TEXT NOT NULL, FCardiaca INTEGER NOT NULL, DRecorrida INTEGER NOT NULL, Correo TEXT NOT NULL)");
 
-  module.exports= db;
+  module.exports= {
+    insert: function (Nombre, Codigo, Precio, Descripcion, FCardiaca, DRecorrida, Correo){
+        db.run("INSERT INTO Productos (Nombre, Codigo, Precio, Descripcion, FCardiaca, DRecorrida, Correo) VALUES (?, ?, ?, ?, ?, ?, ?)", [Nombre, Codigo, Precio, Descripcion, FCardiaca, DRecorrida, Correo], function (err) {
+            if (err){
+                return console.log(err.message);
+            }
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+        });
+    }
+  },
+
+  select= function (callback) {
+    db.all("SELECT * FROM productos", [], (err,rows) => {
+        if (err) {
+            throw err;
+        }
+        callback(rows);
+    });
+  }  

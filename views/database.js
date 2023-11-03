8aed38c4ec66cdd5e377a71cc3088ee276da78e3
Rhyper22 <147758052+Rhyper22@users.noreply.github.com>
2023-11-03 10:25:21 -0400

    Add files via upload
    conexion bd

--- views/database.js
+++ views/database.js
@@ -1,1 +1,11 @@
+const mysql = require('mysql');
 
+const conexion = mysql.createConnection({
+    host:'https://rhypers-system.onrender.com/',
+    user: process.env.USER_ADMIN,
+    password: process.env.PASS_ADMIN,
+    database: 'Proyecto'
+
+})
+
+conexion.connect

171607e7ec356bcf60b139c1ab377c2c8a0079ec
Rhyper22 <147758052+Rhyper22@users.noreply.github.com>
2023-11-04 03:43:58 -0400

    Add files via upload
    definitelyyy

--- database/conexion.js
+++ database/conexion.js
@@ -4,7 +4,7 @@ const db = new sqlite3.Database('./database/database.db', (err) => {
         console.error(err.message);
     }});
 
-    db.run('CREATE TABLE IF NOT EXISTS Productos (id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT, Codigo TEXT, Precio TEXT, Descripcion TEXT, FCardiaca TEXT, DRecorrida TEXT, Correo TEXT, categoria_id INTEGER, FOREIGN KEY (categoria_id) REFERENCES categorias(id))');
+    db.run('CREATE TABLE IF NOT EXISTS Productos (id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT, Codigo INTEGER, Precio INTEGER, Descripcion TEXT, FCardiaca INTEGER, DRecorrida INTEGER, Correo TEXT, categoria_id INTEGER, FOREIGN KEY (categoria_id) REFERENCES categorias(id))');
     db.run('CREATE TABLE IF NOT EXISTS Categorias (id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT)');
     db.run('CREATE TABLE IF NOT EXISTS Images (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, Productos_id INTEGER, FOREIGN KEY (Productos_id) REFERENCES Productos(id))');
     console.log('Base de datos conectada');

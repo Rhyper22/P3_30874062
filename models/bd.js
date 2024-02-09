eb1a21ba06ad677910fdda75ca6d90d93790a9a6
Rhyper22 <147758052+Rhyper22@users.noreply.github.com>
2024-02-08 21:49:30 -0400

    Update bd.js
    d

--- models/bd.js
+++ models/bd.js
@@ -773,7 +773,7 @@ function enviarEmailRecuperacion(req,res){
  res.cookie('securityToken',token, { httpOnly: true, secure: true });
 
   // Crear la URL de recuperación de contraseña
-  const recoveryURL = `http://ventasonlinerandis.onrender.com/restablecer?token=${token}`;
+  const recoveryURL = `https://webfunesrafael.onrender.com/restablecer?token=${token}`;
 
   // Enviar el correo electrónico de recuperación de contraseña
   const mailOptions = {
